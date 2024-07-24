d29a2d7acc017a984aafbcc3bfeb501e8c945bb5
byLugos <bylugos147@gmail.com>
2024-07-23 22:36:52 -0500

    Mejora Final Nav-bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,16 +7,22 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <Link to="/gyms" className="navbar-item">
-        <img src="Gym.png" width='30px' alt="Gym" />
-        <span className='text-navbar-item'>Gimnasios</span>
+        <div className="image-container">
+          <img src="Gym.png" width='30px' alt="Gym" />
+        </div>
+        <span className="text-navbar-item">Gimnasios</span>
       </Link>
       <Link to="/products" className="navbar-item">
-        <img src="Gym.png" width='30px' alt="Products" />
-        <span  className='text-navbar-item' >Productos</span>
+        <div className="image-container">
+          <img src="Market.png" width='30px' alt="Products" />
+        </div>
+        <span className="text-navbar-item">Productos</span>
       </Link>
       <Link to="/account" className="navbar-item">
-        <img src="Gym.png" width='30px' alt="Account" />
-        <span  className='text-navbar-item'>Cuenta</span>
+        <div className="image-container">
+          <img src="User.png" width='30px' alt="Account" />
+        </div>
+        <span className="text-navbar-item">Cuenta</span>
       </Link>
     </div>
   );
